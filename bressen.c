95ba0609b7d8a5e8ed6a34c3dae0563cc174117e
Francisco Jose Moran Mato <fmoran-m@c3r11s2.42madrid.com>
2023-12-20 16:01:54 +0100

    Escribe líneas cortas y rectas, pero no llega a unir los puntos elevados

--- bressen.c
+++ bressen.c
@@ -2,59 +2,52 @@
 
 void	bressen(t_data img, int x0, int x1, int y0, int y1)
 {
-	int	x_vector;
-	int	y_vector;
-	int	x_sen;
-	int	y_sen;
-	int	error;
-	
-	x_sen = 0;
-	y_sen = 0;
-	x_vector = x1 - x0;
-	y_vector = y1 - y0;
-	error = 0;
+	int	dx = x1 - x0;
+	int dy = y1 - y0;
+	int	sx = 0;
+	int	sy = 0;
+	int avr;
+	int	av;
+	int	avi;
+	int	temp;
 
-	if (x_vector > 0)
-		x_sen = 1;
-	else if (x_vector < 0)
-		x_sen = -1;
-	if (y_vector > 0)
-		y_sen = 1;
-	else if (y_vector < 0)
-		y_sen = -1;
-	if (x_vector < 0)
-		x_vector = -x_vector;
-	if (y_vector < 0)
-		y_vector = -y_vector;
-	if (x_vector > y_vector)
+	temp = 0;
+	if (dx < 0)
 	{
-		error = 2 * y_vector - x_vector; 
-		while (x0 < x1 && y0 < y1)
+		sx = -1;
+		dx = -dx; 
+	}
+	else if (dx > 0)
+		sx = 1;
+	if (dy < 0)
+	{
+		sy = -1;
+		dy = -dy;	
+	}
+	else if (dy > 0)
+		sy = 1;
+	if (dy > dx)
+	{
+		dy = temp;
+		dy = dx;
+		dx = temp;
+	}
+	avr = (2 * dy);
+	av = (avr - dx);
+	avi = (av - dx);
+	while (x0 < x1 && y0 < y1)
+	{
+		put_img_pixel(img, x0, y0, MAIN_COLOR);
+		if (av >= 0)
 		{
-			put_img_pixel(img, x0, y0, MAIN_COLOR);
-			if (error >= 0)
-			{
-				y0 = y0 + y_sen;
-				error -= 2 * x_vector; 
-			}
-			x0 = x0 + x_sen;
-			error += y_vector;
+			y0 += sy;
+			x0 += sx;
+			av += avi;
+		}
+		else
+		{
+			x0 += sx;
+			av += avr;
 		}
 	}
-	else
-	{
-		error = 2 * y_vector - x_vector; 
-		while (x0 < x1 && y0 < y1)
-		{
-			put_img_pixel(img, x0, y0, MAIN_COLOR);
-			if (error >= 0)
-			{
-				x0 = x0 + x_sen;
-				error -= 2 * y_vector; 
-			}
-			y0 = y0 + y_sen;
-			error += y_vector;
-		}
-	}
-	put_img_pixel(img, x1, y1, MAIN_COLOR);
 }
