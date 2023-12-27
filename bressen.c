be11f10815d93bb89443fe01fe65491d3b9f31b6
Francisco Jose Moran Mato <fmoran-m@c3r5s2.42madrid.com>
2023-12-27 20:11:34 +0100

    Bressen algorithm torcido

--- bressen.c
+++ bressen.c
@@ -1,5 +1,7 @@
 #include "fdf.h"
+
 void	bressen(t_data img, int x0, int x1, int y0, int y1)
+/*
 {
 	int	dx = x1 - x0;
 	int dy = y1 - y0;
@@ -47,6 +49,58 @@ void	bressen(t_data img, int x0, int x1, int y0, int y1)
 		{
 			x0 += sx;
 			av += avr;
+		}
+	}
+}
+*/
+{
+	int	dx = x1 - x0;
+	int	dy = y1 - y0;
+	int x_flag = 1;
+	int	y_flag = 1;
+	int	x_error = 0;
+	int	y_error = 0;
+	int	temp;
+	int	av;
+	int	avr;
+	int	avi;
+
+	if (dx < 0)
+	{
+		x_flag = -1;
+		dx = -dx;
+	}
+	if (dy < 0)
+	{
+		y_flag = -1;
+		dy = -dy;
+	}
+	if (dx >= dy)
+		x_error = x_flag;
+	else
+	{
+		y_error = y_flag;
+		temp = dx;
+		dx = dy;
+		dy = temp;
+	}
+	avr = (dy * 2);
+	av = (avr - dx);
+	avi = (av - dx);
+	while (y0 != y1 && x0 != x1)
+	{
+		put_img_pixel(img, x0, y0, MAIN_COLOR);
+		if (av >= 0)
+		{
+			x0 += x_error;
+			y0 += y_error;
+			av = av + avi;
+		}
+		else
+		{
+			x0 += x_flag;
+			y0 += y_flag;
+			av = av + avr;
 		}
 	}
 }
