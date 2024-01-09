79dc890d4059a2f6b8c7155df4f448a98d5462d1
Francisco Jose Moran Mato <fmoran-m@c3r11s4.42madrid.com>
2024-01-09 15:57:31 +0100

    Ya imprime

--- parse_line.c
+++ parse_line.c
@@ -22,18 +22,47 @@ static int	get_color(char *str, int i)
 	return (color);
 }
 
+static t_node	rotate_node(t_node new)
+{
+	int x;
+	int y;
+	double theta;
+
+	theta = (-90 * M_PI) / 180;
+	x = (new.x * cos(theta)) - (new.y * sin(theta));
+	y = (new.x * sin(theta)) + (new.x * cos(theta));
+	new.x = x;
+	new.y = y;
+	return (new);
+}
+
+static t_node	iso_node(t_node new)
+{
+	int		x;
+	int		y;
+	double	theta;
+
+	theta = (30 * M_PI) / 180;
+	x = (new.x * cos(theta)) - (new.y * cos(theta)) + 500;
+	y = ((new.x * sin(theta)) + (new.y * sin(theta)) - new.z) + 400;
+	new.x = x;
+	new.y = y;
+	return (new);
+}
+
 t_node	parse_line(char *temp, int x, int y, int x_counter)
 {
 	int		i;
 	int		color;
-	int		x_space = SCREEN_WIDTH / x_counter;
+	int		x_space;
 	t_node	new;
 
+	x_space = (SCREEN_WIDTH / 5) / x_counter;
 	i = 0;
 	color = 0;
-	new.x = x * x_space + 1;
-	new.z = ft_atoi(temp) * x_space + 1;
-	new.y = y * x_space + 1;
+	new.x = x * x_space;
+	new.z = ft_atoi(temp) * x_space;
+	new.y = y * x_space;
 	while (temp[i] && temp[i] != ',')
 		i++;
 	if (temp[i] == ',')
@@ -42,5 +71,8 @@ t_node	parse_line(char *temp, int x, int y, int x_counter)
 		color = get_color(temp, i);
 	}
 	new.color = color;
+	new = rotate_node(new);
+	new = iso_node(new);
+	printf("%d, %d\n", new.x, new.y);
 	return (new);
 }
